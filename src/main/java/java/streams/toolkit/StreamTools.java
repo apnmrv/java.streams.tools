30fb9b6fdb415be9a50057f4219bef39e41ed2cb
Andrey Ponomarev <apnmrv@gmail.com>
2020-04-11 14:03:15 +0300

    initial

--- src/main/java/java/streams/toolkit/StreamTools.java
+++ src/main/java/java/streams/toolkit/StreamTools.java
@@ -1,4 +1,4 @@
-package local.streams.sandbox;
+package java.streams.toolkit;
 
 import java.util.Collection;
 import java.util.stream.Stream;
--- src/test/java/java/streams/toolkit/StreamToolsTest.java
+++ src/test/java/java/streams/toolkit/StreamToolsTest.java
@@ -1,4 +1,4 @@
-package local.streams.sandbox;
+package java.streams.toolkit;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
